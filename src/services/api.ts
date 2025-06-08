eb1a7a48577e26b2dfe6fdfc9e10d4ca02fa3bae
deepbhut22 <deeppatel6071@gmail.com>
2025-06-08 23:12:12 +0530

    initial commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -16,7 +16,9 @@ import {
 } from '@/types';
 import { toast } from 'sonner';   
 import { useAuthStore } from '@/stores/authStore';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://saasapi.pathpr.ca/api';
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
